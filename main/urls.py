5a672eec9d721d5bf59bc658569f3e012080229c
Jonathan Yitskhaq Rundjan <jonathanrundjan@Ishaks-Laptop.local>
2025-09-24 15:45:53 +0700

    Done tutorial 4

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from main.views import register
 from main.views import login_user
 from main.views import logout_user
+from main.views import edit_news
+from main.views import delete_news
 
 app_name = 'main'
 
@@ -17,6 +19,8 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
+    path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete', delete_news, name='delete_news'),
 
 
 
